45fd0048e584417c4c84cafa8bd39352d0cc3d95
Yalcin <yalcin.coding@gmail.com>
2023-07-18 09:18:25 +0200

    Rechner

--- script_01.js
+++ script_01.js
@@ -8,17 +8,44 @@
 1. Dateneingabe + -überprüfung : 
 2. Auswahl Rechenart : 
 3. Fkt. Grundrechenarten : 
-4. Ausgabe in Konsole : 
+4. Ausgabe in Konsole : check!
 */
 
 
 
 
 
+
+
+// module: addition a + b | test:
+
+
+
+
+
+
 // module: output | test:
-// output("hello");
-// output(2);
+
 function output(outputData) {
 	console.log(outputData);
 }
-  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num1 = parseFloat(prompt("Gib eine Zahl ein:"));
+// let num2 = parseFloat(prompt("Gib die zweite Zahl ein:"));
+
+// let outputData = num1 + num2;
+
+// console.log(outputData);
