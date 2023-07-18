472d3ae39a4869d42a16bfa145d1acd7501e9d69
Yalcin <yalcin.coding@gmail.com>
2023-07-18 12:18:11 +0200

    finish

--- script_01.js
+++ script_01.js
@@ -6,79 +6,84 @@
 0. a+b | a-b | a*b | a/b  --> Ergebnis c 
 
 1. Dateneingabe + -überprüfung : 
-2. Auswahl Rechenart : 
-3. Fkt. Grundrechenarten : 
+2. Auswahl Rechenart : check!
+3. Fkt. Grundrechenarten : check!
 4. Ausgabe in Konsole : check!
 */
 
+const ERROR_STR_DIV = "Division durch 0 nicht möglich!";
+const ERROR_STR_GEN = "Irgendwas ging schief!";
 
-const ERROR_STR_DIV = "Division durch 0 nicht möglich";
+// module: calculator | tests:
+// agreement : "+","-","*",":","/"
+output(calculator(3,2,"+"));
+output(calculator(3,2,"-"));
+output(calculator(3,2,"*"));
+output(calculator(3,2,":"));
+output(calculator(3,2,"/"));
+output(calculator(3,0,"/"));
+output(calculator(3,2,"#?!"));
+function calculator(a,b,op) {
+	switch (op) {
+		case "+":
+			return add(a,b);
+		case "-":
+			return subtract(a,b);
+		case "*":
+			return multiply(a,b);
+		case ":":
+		case "/":
+			return divide(a,b);
+		default:
+			return ERROR_STR_GEN;
+	}
+}
 
 // module: division a / b |  test:
-// output(devide(4, 2));
-// output(devide(3, 2));
-// output(devide(3, -2));
-// output(devide(0, 2));
-// output(devide(3, 0));
-function devide(a, b)
-{
+// output(divide(4,2));
+// output(divide(3,2));
+// output(divide(3,-2));
+// output(divide(0,2));
+// output(divide(3,0));
+function divide(a,b) {
 
-	// if (b != 0) {
-	// 	return a / b;
-	// } else {
-	// 	return "Division durch 0 nicht möglich";
-	// }
-
-	if (b == 0) {	// Ausnahme + Abbruch
+	if (b == 0) { // Ausnahme + Abbruch
 		return ERROR_STR_DIV;
 	}
+	
+	return a/b; // Regel
 
-	return a / b;	// Regelfall
 }
-
-
 
 // module: multiplication a * b |  test:
-
-// output(add(3, 2));
-// output(add(3, -2));
-// output(add(3, 0));
-function multiply(a, b)
-{
-	return a * b;
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
+function multiply(a,b) {
+	return a*b;
 }
 
-
-
 // module: subtraction a - b |  test:
-
-// output(subtract(3, 2));
-// output(subtract(3, -2));
-// output(subtract(3, 0));
-// output(subtract(0, 3));
-function subtract(a, b)
-{
+// output(subtract(3,2));
+// output(subtract(3,-2));
+// output(subtract(3,0));
+// output(subtract(0,3));
+function subtract(a,b) {
 	return a - b;
 }
 
-
-
 // module: addition a + b |  test:
-
-// output(add(2, 2));
-// output(add(2, -2));
-// output(add(2, 0));
-function add(a, b)
-{
+// output(add(2,2));
+// output(add(2,-2));
+// output(add(2,0));
+function add(a,b) {
 	return a + b;
 }
-
-
 
 // module: output | test:
 // output("hello");
 // output(2);
-function output(outputData)
-{
+function output(outputData) {
 	console.log(outputData);
 }
+  
